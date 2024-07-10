9ccff12f81d5793d7c25a663b65d0359a93d8fd1
HoyeongJeon <hoyeong0302@gmail.com>
2024-07-10 18:23:18 +0900

    feat: 회원가입

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,140 +1,156 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
-  const [name, setName] = useState('');
-  const [studentNumber, setStudentNumber] = useState('');
-  const [major, setmajor] = useState('');
-  const [birthDate, setBirthDate] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [verificationCode, setVerificationCode] = useState('');
+  const [name, setName] = useState("");
+  const [studentNumber, setStudentNumber] = useState("");
+  const [major, setmajor] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
   const [verificationSent, setVerificationSent] = useState(false);
-  const [verificationError, setVerificationError] = useState('');
+  const [verificationError, setVerificationError] = useState("");
   const [isSignupComplete, setIsSignupComplete] = useState(false);
+
+  const navigate = useNavigate();
 
   const styles = {
     container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh',
-      backgroundColor: '#3a5ba0',
-      color: 'white',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100vh",
+      backgroundColor: "#3a5ba0",
+      color: "white",
     },
     form: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      width: '300px',
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "300px",
     },
     inputField: {
-      width: '100%',
-      padding: '10px',
-      margin: '10px 0',
-      border: '1px solid #ccc',
-      borderRadius: '5px',
-      fontSize: '16px',
+      width: "100%",
+      padding: "10px",
+      margin: "10px 0",
+      border: "1px solid #ccc",
+      borderRadius: "5px",
+      fontSize: "16px",
     },
     button: {
-      width: '100%',
-      padding: '10px',
-      margin: '10px 0',
-      border: 'none',
-      borderRadius: '20px',
-      fontSize: '16px',
-      cursor: 'pointer',
-      backgroundColor: '#fff',
-      color: '#3a5ba0',
-      fontWeight: 'bold',
+      width: "100%",
+      padding: "10px",
+      margin: "10px 0",
+      border: "none",
+      borderRadius: "20px",
+      fontSize: "16px",
+      cursor: "pointer",
+      backgroundColor: "#fff",
+      color: "#3a5ba0",
+      fontWeight: "bold",
     },
     emailButtonContainer: {
-      display: 'flex',
-      justifyContent: 'flex-end',
-      width: '100%',
-      marginBottom: '10px',
+      display: "flex",
+      justifyContent: "flex-end",
+      width: "100%",
+      marginBottom: "10px",
     },
     errorMessage: {
-      color: 'red',
-      fontSize: '14px',
+      color: "red",
+      fontSize: "14px",
     },
     link: {
-      textDecoration: 'none',
-      color: '#3a5ba0',
-      fontWeight: 'bold',
-      backgroundColor: '#fff',
-      padding: '10px 20px',
-      borderRadius: '20px',
-      marginTop: '10px',
+      textDecoration: "none",
+      color: "#3a5ba0",
+      fontWeight: "bold",
+      backgroundColor: "#fff",
+      padding: "10px 20px",
+      borderRadius: "20px",
+      marginTop: "10px",
     },
   };
 
   const handleEmailVerification = () => {
-    console.log('이메일 인증 클릭됨');
-    axios.post('http://localhost:3000/auth/create-verification-code', {
-      studentNumber: studentNumber,
-    }, {
-      withCredentials: true
-    })
-    .then(response => {
-      console.log('인증 코드 전송 성공:', response.data);
-      setVerificationSent(true);
-      setVerificationError(''); // 이메일 인증 코드가 성공적으로 전송됨을 설정합니다.
-    })
-    .catch(error => {
-      console.error('인증 코드 전송 오류:', error);
-      alert('인증 코드 전송에 실패했습니다. 다시 시도해주세요.');
-    });
+    console.log("이메일 인증 클릭됨");
+    axios
+      .post(
+        "http://localhost:3000/auth/create-verification-code",
+        {
+          studentNumber: studentNumber,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        console.log("인증 코드 전송 성공:", response.data);
+        setVerificationSent(true);
+        setVerificationError(""); // 이메일 인증 코드가 성공적으로 전송됨을 설정합니다.
+      })
+      .catch((error) => {
+        console.error("인증 코드 전송 오류:", error);
+        alert("인증 코드 전송에 실패했습니다. 다시 시도해주세요.");
+      });
   };
 
   const handleVerificationSubmit = () => {
-    axios.post('http://localhost:3000/auth/is-verified', {
-      studentNumber: studentNumber,
-      codeFromUser: verificationCode
-    }, {
-      withCredentials: true
-    })
-    .then(response => {
-      console.log(response);
-      if (response.data == true) {
-        alert('인증이 완료되었습니다!');
-        setVerificationError('');
-      } else {
-        setVerificationError('인증번호가 일치하지 않습니다.');
-      }
-    })
-    .catch(error => {
-      console.error('인증 오류:', error);
-      setVerificationError('인증번호가 일치하지 않습니다.');
-    });
+    axios
+      .post(
+        "http://localhost:3000/auth/is-verified",
+        {
+          studentNumber: studentNumber,
+          codeFromUser: verificationCode,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        if (response.data === true) {
+          alert("인증이 완료되었습니다!");
+          setVerificationError("");
+        } else {
+          setVerificationError("인증번호가 일치하지 않습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("인증 오류:", error);
+        setVerificationError("인증번호가 일치하지 않습니다.");
+      });
   };
 
   const handleSignupSubmit = (e) => {
     e.preventDefault();
     if (!name || !major || !studentNumber || !birthDate || !phoneNumber) {
-      alert('모든 정보를 입력해주세요!');
+      alert("모든 정보를 입력해주세요!");
       return;
     }
 
-    axios.post('http://localhost:3000/auth/signup', {
-      name: name,
-      major: major,
-      studentNumber: studentNumber,
-      birth: birthDate,
-      phone: phoneNumber
-    }, {
-      withCredentials: true
-    })
-    .then(response => {
-      console.log('회원가입 성공:', response.data);
-      alert('회원가입이 완료되었습니다!');
-      setIsSignupComplete(true); // 회원가입 성공 시 상태 업데이트
-    })
-    .catch(error => {
-      console.error('회원가입 오류:', error);
-      alert('회원가입에 실패했습니다. 다시 시도해주세요.');
-    });
+    axios
+      .post(
+        "http://localhost:3000/auth/signup",
+        {
+          name: name,
+          major: major,
+          studentNumber: +studentNumber,
+          birth: birthDate,
+          phone: phoneNumber,
+        },
+        {
+          withCredentials: true,
+        }
+      )
+      .then((response) => {
+        alert("회원가입이 완료되었습니다!");
+        navigate("/login");
+        // setIsSignupComplete(true); // 회원가입 성공 시 상태 업데이트
+      })
+      .catch((error) => {
+        console.error("회원가입 오류:", error);
+        alert(`${error.errorMessage}`);
+      });
   };
 
   return (
@@ -148,18 +164,24 @@ function Signup() {
           onChange={(e) => setName(e.target.value)}
           style={styles.inputField}
         />
-        <div style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+        <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
           <input
             type="text"
             placeholder="학번"
             value={studentNumber}
             onChange={(e) => setStudentNumber(e.target.value)}
-            style={{ ...styles.inputField, marginRight: '10px' }}
+            style={{ ...styles.inputField, marginRight: "10px" }}
           />
-          <span style={{ fontSize: '16px', color: 'white' }}>@dankook.ac.kr</span>
+          <span style={{ fontSize: "16px", color: "white" }}>
+            @dankook.ac.kr
+          </span>
         </div>
         <div style={styles.emailButtonContainer}>
-          <button type="button" style={{ ...styles.button, minWidth: '120px' }} onClick={handleEmailVerification}>
+          <button
+            type="button"
+            style={{ ...styles.button, minWidth: "120px" }}
+            onClick={handleEmailVerification}
+          >
             이메일 인증
           </button>
         </div>
@@ -172,13 +194,21 @@ function Signup() {
               onChange={(e) => setVerificationCode(e.target.value)}
               style={styles.inputField}
             />
-            <button type="button" style={styles.button} onClick={handleVerificationSubmit}>
+            <button
+              type="button"
+              style={styles.button}
+              onClick={handleVerificationSubmit}
+            >
               인증번호 확인하기
             </button>
             {verificationError && (
               <div style={styles.errorMessage}>{verificationError}</div>
             )}
-            <button type="button" style={styles.button} onClick={handleEmailVerification}>
+            <button
+              type="button"
+              style={styles.button}
+              onClick={handleEmailVerification}
+            >
               재전송
             </button>
           </>
@@ -204,7 +234,11 @@ function Signup() {
           onChange={(e) => setPhoneNumber(e.target.value)}
           style={styles.inputField}
         />
-        <button type="button" style={styles.button} onClick={handleSignupSubmit}>
+        <button
+          type="button"
+          style={styles.button}
+          onClick={handleSignupSubmit}
+        >
           회원가입하기
         </button>
         {isSignupComplete && (
