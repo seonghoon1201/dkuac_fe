8e8135b817754355c1654abb96da520c0a2e21a3
[seonghoon] <[32194096.dankook.ac.kr]>
2024-07-22 17:27:24 +0900

    rent_shoes

--- src/pages/Equipment/index.js
+++ src/pages/Equipment/index.js
@@ -3,111 +3,140 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./styles";
 import userInfoStore from "../../stores/userInfoStore";
+import { basicAxios, authAxios } from "../../api/axios";
 
 const Equipment = () => {
-    const { isLoggedIn } = userInfoStore();
-    const [showPopup, setShowPopup] = useState(false);
-    const [showLoginPopup, setShowLoginPopup] = useState(false);
-    const [selectedSize, setSelectedSize] = useState(null);
-    const [sizes, setSizes] = useState([]);
-    const navigate = useNavigate();
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await axios.get(`http://localhost:3000/rent`);
-          console.log(response);
-          setSizes(response.data); // 백엔드에서 받은 데이터를 sizes 상태로 설정
-        } catch (error) {
-          console.error("데이터를 가져오는 데 실패했습니다:", error);
-        }
-      };
-  
-      fetchData();
-    }, []);
-  
-    const handlePopupOpen = () => {
-      if (isLoggedIn) {
-        setShowPopup(true);
-      } else {
-        setShowLoginPopup(true);
+  const { isLoggedIn } = userInfoStore();
+  const [showPopup, setShowPopup] = useState(false);
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const [selectedSize, setSelectedSize] = useState(null);
+  const [sizes, setSizes] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await basicAxios.get(`/rent`);
+        setSizes(response.data); // 백엔드에서 받은 데이터를 sizes 상태로 설정
+      } catch (error) {
+        console.error("데이터를 가져오는 데 실패했습니다:", error);
       }
     };
-  
-    const handlePopupClose = () => {
-      setShowPopup(false);
-    };
-  
-    const handleLoginPopupClose = () => {
-      setShowLoginPopup(false);
-      navigate("/login");
-    };
-  
-    const handleSizeSelect = (size) => {
-      setSelectedSize(size);
-      handlePopupClose();
-    };
-  
-    return (
-      <div>
-        <div style={styles.container}>
-          <h1 style={styles.title}>암벽화 보유 현황</h1>
-          <p style={styles.description}>
-            암벽화는 정사이즈로 신으셔야 편합니다. 암벽화 착화 시 발가락이 조금 구부려져야 맞는 사이즈를 착화하신 것이니 참고 부탁드립니다.
-            암벽화가 너무 작으면 발이 많이 아프고 암벽화가 크면 발로 홀드를 잡을 수 없기 때문에 본인에게 맞는 사이즈로 대여 부탁드립니다.
-          </p>
-          <table style={styles.table}>
-            <thead>
-              <tr>
-                <th style={styles.tableHeader}>사이즈</th>
-                <th style={styles.tableHeader}>총 개수</th>
-                <th style={styles.tableHeader}>대여 중</th>
-                <th style={styles.tableHeader}>남은 개수</th>
+
+    fetchData();
+  }, []);
+
+  const handlePopupOpen = () => {
+    if (isLoggedIn) {
+      setShowPopup(true);
+    } else {
+      setShowLoginPopup(true);
+    }
+  };
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
+
+  const handleLoginPopupClose = () => {
+    setShowLoginPopup(false);
+    navigate("/login");
+  };
+
+  const handleSizeSelect = (size) => {
+    setSelectedSize(size);
+  };
+
+  const handleRent = async () => {
+    if (selectedSize) {
+      console.log(selectedSize);
+      try {
+        const response = await authAxios.post(`/rent`, {
+          size: selectedSize,
+        });
+        console.log(response.data);
+        alert("대여가 완료되었습니다!");
+        handlePopupClose();
+      } catch (error) {
+        //console.error("대여 요청에 실패했습니다:", error);
+        console.error(error.message)
+        alert("대여 요청에 실패했습니다. 다시 시도해주세요.");
+      }
+    } else {
+      alert("사이즈를 선택해주세요.");
+    }
+  };
+
+  return (
+    <div>
+      <div style={styles.container}>
+        <h1 style={styles.title}>암벽화 보유 현황</h1>
+        <p style={styles.description}>
+          암벽화는 정사이즈로 신으셔야 편합니다. 암벽화 착화 시 발가락이 조금 구부려져야 맞는 사이즈를 착화하신 것이니 참고 부탁드립니다.
+          암벽화가 너무 작으면 발이 많이 아프고 암벽화가 크면 발로 홀드를 잡을 수 없기 때문에 본인에게 맞는 사이즈로 대여 부탁드립니다.
+        </p>
+        <table style={styles.table}>
+          <thead>
+            <tr>
+              <th style={styles.tableHeader}>사이즈</th>
+              <th style={styles.tableHeader}>총 개수</th>
+              <th style={styles.tableHeader}>대여 중</th>
+              <th style={styles.tableHeader}>남은 개수</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sizes.map((item) => (
+              <tr key={item.size}>
+                <td style={styles.tableCell}>{item.size}</td>
+                <td style={styles.tableCell}>{item.count}</td>
+                <td style={styles.tableCell}>{item.count - item.rentable}</td>
+                <td style={styles.tableCell}>{item.rentable}</td>
               </tr>
-            </thead>
-            <tbody>
-              {sizes.map((item) => (
-                <tr key={item.size}>
-                  <td style={styles.tableCell}>{item.size}</td>
-                  <td style={styles.tableCell}>{item.count}</td>
-                  <td style={styles.tableCell}>{item.count - item.rentable}</td>
-                  <td style={styles.tableCell}>{item.rentable}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <button style={styles.button} onClick={handlePopupOpen}>암벽화 대여하기</button>
-          {showPopup && (
-            <div style={styles.popupOverlay}>
-              <div style={styles.popup}>
-                <h2 style={styles.popupTitle}>사이즈 선택</h2>
-                <div style={styles.popupContent}>
-                  {sizes.map((item) => (
-                    <button 
-                      key={item.size} 
-                      style={styles.sizeButton} 
-                      onClick={() => handleSizeSelect(item.size)}
-                    >
-                      {item.size}
-                    </button>
-                  ))}
-                </div>
+            ))}
+          </tbody>
+        </table>
+        <button style={styles.button} onClick={handlePopupOpen}>암벽화 대여하기</button>
+        {showPopup && (
+          <div style={styles.popupOverlay}>
+            <div style={styles.popup}>
+              <h2 style={styles.popupTitle}>사이즈 선택</h2>
+              <p style={styles.noticeText}>암벽화 대여하신 분들은 모두 이번 주 금요일까지 암벽화 반납 부탁드립니다.</p>
+              <div style={styles.popupContent}>
+                {sizes.map((item) => (
+                  <button 
+                    key={item.size} 
+                    style={{
+                      ...styles.sizeButton,
+                      backgroundColor: selectedSize === item.size ? 'blue' : item.rentable <= 0 ? 'gray' : 'initial', // 선택된 버튼과 비활성화된 버튼 색상 변경
+                      color: selectedSize === item.size ? 'white' : item.rentable <= 0 ? 'lightgray' : 'initial', // 선택된 버튼과 비활성화된 버튼 텍스트 색상 변경
+                      cursor: item.rentable <= 0 ? 'not-allowed' : 'pointer' // 비활성화된 버튼 커서 스타일 변경
+                    }} 
+                    onClick={() => handleSizeSelect(item.size)}
+                    disabled={item.rentable <= 0} // 대여 가능한 암벽화가 없을 경우 버튼 비활성화
+                  >
+                    {item.size}
+                  </button>
+                ))}
+              </div>
+              <div style={styles.buttonContainer}>
+                <button style={styles.rentButton} onClick={handleRent}>대여하기</button>
                 <button style={styles.closeButton} onClick={handlePopupClose}>닫기</button>
               </div>
             </div>
-          )}
-          {showLoginPopup && (
-            <div style={styles.popupOverlay}>
-              <div style={styles.popup}>
-                <h2 style={styles.warnTitle}>로그인이 필요합니다</h2>
-                <p style={styles.warnContent}>로그인을 먼저 해주세요!</p>
-                <button style={styles.closeButton} onClick={handleLoginPopupClose}>로그인 페이지로 이동</button>
-              </div>
+          </div>
+        )}
+        {showLoginPopup && (
+          <div style={styles.popupOverlay}>
+            <div style={styles.popup}>
+              <h2 style={styles.warnTitle}>로그인이 필요합니다</h2>
+              <p style={styles.warnContent}>로그인을 먼저 해주세요!</p>
+              <button style={styles.closeButton} onClick={handleLoginPopupClose}>로그인 페이지로 이동</button>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
-    );
-  };
-  
-  export default Equipment;  
+    </div>
+  );
+};
+
+export default Equipment;
